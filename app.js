6128f33d389001d1c3dd20376d868a83147dcde8
Mikael Wallin <mikael.x.wallin@gmail.com>
2022-01-15 04:31:50 +0100

    Working version

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 document.addEventListener("submit" , (event)=>{
-    console.log("Submitted")
-    user = document.getElementById("user").value
-    password = document.getElementById("password").value
-    
-    const headers = new Headers()
-    headers.append("Accept" , "application/json")
-    headers.append("Authorization", btoa(user + ":" + password))
+    console.log("Validating")
+    let user = document.getElementById("user").value;
+    let password = document.getElementById("password").value;
+
+    let request = new XMLHttpRequest();
+    request.open("POST", "http://localhost:3000/authorize");
+    //request.send(new FormData(formElement));
+    console.log("Validated")
+
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    request.send(`user=${user}&password=${password}`);
 
     event.preventDefault()
 })
-
-
-function validate(){
-    console.log("Validating")
-    let user = document.getElementById("user").value
-    let password = document.getElementById("password").value
-    console.log(`User = ${user} Password  = ${password}`)
-
-    const XHR = new XMLHttpRequest(), FD  = new FormData();
-    FD.append( "user", user);
-    FD.append( "password", password );    
-    XHR.open( 'POST', 'http://localhost:3000/authorize' );
-    XHR.send( FD );
-    console.log("Validated")
-}
